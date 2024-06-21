fa042cb4bb3fa9b660d9d3b86b12c0d672550059
zoroxzoro <Kaifalam9580@gmail.com>
2024-06-21 13:40:47 +0530

    final commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 interface Todo {
   title: string;
   isCompleted: boolean;
-  readonly id: number;
+  readonly id: string;
 }
 
 const todos: Todo[] = [];
@@ -21,10 +21,47 @@ myForm.onsubmit = (e: SubmitEvent) => {
   const newTodo: Todo = {
     title: todoInput.value,
     isCompleted: false,
-    id: Math.random() * 100,
+    id: String(Math.random() * 100),
   };
   todos.push(newTodo);
   todoInput.value = "";
-  renderTodo();
+  renderTodo(todos);
 };
-const renderTodo = () => {};
+
+const generateTodos = (title: string, isCompleted: boolean, id: string) => {
+  const todo: HTMLDivElement = document.createElement("div");
+  todo.className = "todo";
+  const checkBox: HTMLInputElement = document.createElement("input");
+  checkBox.setAttribute("type", "checkBox");
+  checkBox.className = "isCompleted";
+  checkBox.checked = isCompleted;
+
+  // creating p
+
+  const paragraph: HTMLParagraphElement = document.createElement("p");
+  checkBox.setAttribute("type", "checkBox");
+  checkBox.className = "isCompleted";
+  paragraph.innerText = title;
+
+  // creating delete button
+  const btn: HTMLButtonElement = document.createElement("button");
+  btn.innerText = "X";
+  btn.className = "deleteBtn";
+  todo.append(checkBox, paragraph, btn);
+  todoContainer.append(todo);
+  btn.onclick = () => {
+    deleteTodo(id);
+  };
+};
+
+const deleteTodo = (id: string) => {
+  const idx = todos.findIndex((items) => items.id === id);
+  todos.splice(idx, 1);
+  renderTodo(todos);
+};
+const renderTodo = (todos: Todo[]) => {
+  todoContainer.innerText = "";
+  todos.forEach((items) => {
+    generateTodos(items.title, items.isCompleted, items.id);
+  });
+};
